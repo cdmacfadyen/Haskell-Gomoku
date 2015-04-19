abd3917af90e0b0a93bbcb2ec08d5dc7cce200b8
bc37 <bc37@st-andrews.ac.uk>
2015-04-19 20:39:59 +0100

    Indentation fixed for commenting(?)

--- src/Board.hs
+++ src/Board.hs
@@ -23,13 +23,13 @@ allTransforms = [transformUp, transformUpRight, transformRight, transformDownRig
 
 -- ^Given a position and a transform, return the position modified by the transform.
 transform :: Position -> -- ^Takes a position to apply transformation to
-				Position -> -- ^Transformation to apply on postion
-				Position -- ^Returns the transformed postion
+             Position -> -- ^Transformation to apply on postion
+             Position -- ^Returns the transformed postion
 transform (x, y) (x_diff, y_diff) = (x + x_diff, y + y_diff)
 
 -- | Switch current player.
 switch :: Colour -> -- ^Takes the colour of the current player
-			Colour -- ^Returns the opposite colour to change turns
+          Colour -- ^Returns the opposite colour to change turns
 switch Black = White
 switch White = Black
 
@@ -71,40 +71,40 @@ data Settings = Settings { grid_size :: Int, -- ^Board Representation
 -- | Play a move on the board; return 'Nothing' if the move is invalid
 -- (e.g. outside the range of the board, or there is a piece already there)
 makeMove :: Board -> -- ^The current Board.
-				Colour -> -- ^The colour of the current player
-				Position -> -- ^The position a piece is trying to be placed
-				Maybe Board -- ^The new board state after attempted move
+            Colour -> -- ^The colour of the current player
+            Position -> -- ^The position a piece is trying to be placed
+            Maybe Board -- ^The new board state after attempted move
 makeMove b col p = if contains p $ pieces b then Nothing else Just newboard {won = checkWon newboard, hint = Nothing}
         where newboard = b {pieces = ((p, col) : (pieces b))}
 
 -- | Given the same arguments as makeMove, return a new board if the move was successful, 
 -- or the original board if it was not.
 maybeMakeMove :: Board -> -- ^Takes the current board
-					Colour -> -- ^The current player's colour
-					Position -> -- ^The position where a piece is trying to be placed
-					Board -- ^Returns a new board state
+                 Colour -> -- ^The current player's colour
+                 Position -> -- ^The position where a piece is trying to be placed
+                 Board -- ^Returns a new board state
 maybeMakeMove b col p = case makeMove b col p of
                              Just board -> board
                              Nothing    -> b
 
 -- | Checks if there is a piece of either colour at the given position
 contains :: Position -> -- ^Takes the position of board where piece is trying to go
-				[Piece] -> -- ^The list of all pieces on the board
-				Bool -- ^Returns whether the position is occupied or not
+            [Piece] -> -- ^The list of all pieces on the board
+            Bool -- ^Returns whether the position is occupied or not
 contains position positions = (position, White) `elem` positions || (position, Black) `elem` positions
 
 -- | Gets a World from original World and Maybe Board
 -- Have to check if Maybe Board is Nothing or Board before returning World
 maybeBoardToWorld :: World -> -- ^Takes the current World state
-						Maybe Board -> -- ^The new Board state to be updated in World
-						World -- ^A new World state with the new Board state
+                     Maybe Board -> -- ^The new Board state to be updated in World
+                     World -- ^A new World state with the new Board state
 maybeBoardToWorld b Nothing = b
 maybeBoardToWorld b (Just mBoard) = b {board = mBoard, turn = switch (turn b), settings = new_settings}
           where new_settings = (settings b){game_in_progress = (is_in_progress_game b)}
 
 -- | Checks if game is still ongoing or not
 is_in_progress_game :: World -> -- ^Takes current World state
-						Bool -- ^True or False depending on whether game is ongoing or not
+                       Bool -- ^True or False depending on whether game is ongoing or not
 is_in_progress_game w = case (won (board w)) of
         Just Black -> False
         Just White -> False
@@ -114,21 +114,21 @@ is_in_progress_game w = case (won (board w)) of
 -- Returns 'Nothing' if neither player has won yet
 -- Returns 'Just c' if the player 'c' has won
 checkWon :: Board -> -- ^Takes current Board state
-			Maybe Colour -- ^Rreturns winning player colour
+            Maybe Colour -- ^Rreturns winning player colour
 checkWon b = checkn b (target b) 
 
 -- | Convenience function for getting the position of the given piece
 getPiecePos :: Piece -> -- ^Takes piece you want the position for
-				Position -- ^Returns position of a piece
+               Position -- ^Returns position of a piece
 getPiecePos (position, _) = position
 
 -- | Given the list of pieces, a transform direction to check, a position of a piece to check, the target number in a row and a colour to check victory for, return Just colour if colour has won, or Nothing otherwise.
 checkTransformColour :: [Piece] -> -- ^Takes list of all pieces on the board
-							Position -> -- ^The transform direction
-							Position -> -- ^Piece position to check
-							Int -> -- ^What the target 'in-a-row' is
-							Colour -> -- ^Colour to check if won
-							Maybe Colour -- ^Return the colour of player if they won
+                        Position -> -- ^The transform direction
+                        Position -> -- ^Piece position to check
+                        Int -> -- ^What the target 'in-a-row' is
+                        Colour -> -- ^Colour to check if won
+                        Maybe Colour -- ^Return the colour of player if they won
 checkTransformColour piecelist (x_diff, y_diff) (x, y) targetN colour
     | ((x, y), colour) `elem` piecelist = if targetN == 0
                                                 then Nothing
@@ -139,11 +139,11 @@ checkTransformColour piecelist (x_diff, y_diff) (x, y) targetN colour
 
 -- | Checks one direction using transformed co-ordinate and returns number of 'n' length lines
 checkTransformColourCount :: [Piece] -> -- ^Takes a list of pieces on the board
-								Position -> -- ^Position to apply transformation to
-								Position -> -- ^Transformation to apply to position
-								Int -> -- ^Target 'in-a-row' to match against
-								Colour -> -- ^Colour to check for
-								Int -- ^The number of lines matching the search
+                             Position -> -- ^Position to apply transformation to
+                             Position -> -- ^Transformation to apply to position
+                             Int -> -- ^Target 'in-a-row' to match against
+                             Colour -> -- ^Colour to check for
+                             Int -- ^The number of lines matching the search
 checkTransformColourCount piecelist (x_diff, y_diff) (x, y) targetN colour
     | ((x, y), colour) `elem` piecelist = if targetN == 0
                                                 then 0
@@ -154,16 +154,16 @@ checkTransformColourCount piecelist (x_diff, y_diff) (x, y) targetN colour
 
 -- | Filters out the positions of the given colour.
 colourFilter :: Board -> -- ^Takes current Board state
-				Colour -> -- ^A colour indicating player, either Black or White
-				[Position] -- ^Returns a list of positions belonging to the given colour
+                Colour -> -- ^A colour indicating player, either Black or White
+                [Position] -- ^Returns a list of positions belonging to the given colour
 colourFilter board colour = map fst $ filter (\(_, col) -> col == colour) (pieces board)
 
 -- | Check whether the board is in a winning state for either player.
 -- Returns 'Nothing' if neither player has won yet
 -- Returns 'Just c' if the player 'c' has won
 checkn :: Board -> -- ^Takes the current Board state
-			Int -> -- ^The target 'in-a-row' needed for a win
-			Maybe Colour -- ^Colour rreturn if won else nothing
+          Int -> -- ^The target 'in-a-row' needed for a win
+          Maybe Colour -- ^Colour rreturn if won else nothing
 checkn b targetN = msum [(checkTransformColour piecelist transform position targetN colour)
                     | (position, colour) <- piecelist,
                       transform <- allTransforms,
@@ -173,9 +173,9 @@ checkn b targetN = msum [(checkTransformColour piecelist transform position targ
 
 -- | Takes a board, a number in a row to check for and a colour to check for, return the number of lines of exactly that length belonging to that player
 countNConnected :: Board -> -- ^Takes the current Board state
-					Int -> -- ^How many rows of 'n' length you want to search for
-					Colour -> -- ^The colour (player) of the pieces you are searching for
-					Int -- ^Returns the number of lines that have been found matching the Int
+                   Int -> -- ^How many rows of 'n' length you want to search for
+                   Colour -> -- ^The colour (player) of the pieces you are searching for
+                   Int -- ^Returns the number of lines that have been found matching the Int
 countNConnected b targetN col = sum [checkTransformColourCount piecelist transform position targetN col
                                     |(position, colour) <- piecelist,
                                      transform <- allTransforms]
@@ -183,13 +183,13 @@ countNConnected b targetN col = sum [checkTransformColourCount piecelist transfo
 
 -- | Works out the number of pixels in a square on the grid
 squareSize :: World -> -- ^Takes current World state
-				Float -- ^Returns the number of pixels in a grid square
+              Float -- ^Returns the number of pixels in a grid square
 squareSize w = fromIntegral (width w) / (fromIntegral . size $ board w)
 
 -- | Given a world and a screen-space co-ordinate, return the closest board position to that co-ordinate if it is in bounds, or Nothing if it is not.
 screenSpaceToBoardSpace :: World -> -- ^Takes the current World state
-							(Float, Float) -> -- ^The position clicked on the board
-							Maybe Position -- ^Returns the nearest valid position on the board, if any
+                           (Float, Float) -> -- ^The position clicked on the board
+                           Maybe Position -- ^Returns the nearest valid position on the board, if any
 screenSpaceToBoardSpace world (screenx, screeny) 
     | boardx >= minbound && boardx <= maxbound && boardy >= minbound && boardy <= maxbound = Just (boardx, boardy)
     | otherwise = Nothing
@@ -209,8 +209,8 @@ screenSpaceToBoardSpace world (screenx, screeny)
 
 -- | Given a world and a board-space co-ordinate position, return the screen-space co-ordinate of that position.
 boardSpaceToScreenSpace :: World -> -- ^Takes the current World state
-							Position -> -- ^A board position given
-							(Float, Float) -- ^The screen space co-ordinate of the board position
+                           Position -> -- ^A board position given
+                           (Float, Float) -- ^The screen space co-ordinate of the board position
 boardSpaceToScreenSpace world (boardx, boardy)
     | even      = (screenx, screeny)
     | otherwise = (screenx + gridsize / 2, screeny + gridsize / 2)
@@ -221,35 +221,35 @@ boardSpaceToScreenSpace world (boardx, boardy)
 
 -- | Checks if the Board is even or not
 is_even :: Board -> -- ^Takes the current Board state
-			Bool -- ^True if even, else False if odd
+           Bool -- ^True if even, else False if odd
 is_even board = (size board) `rem` 2 == 0
 
 -- | Gets the minimum bound of the Board
 get_min :: Board -> -- ^Takes the current Board state
-			Int -- ^Returns minimum bound of Board
+           Int -- ^Returns minimum bound of Board
 get_min board = if is_even board then -halfgridwidth else -halfgridwidth - 1
     where halfgridwidth = (size board) `quot` 2
 
 -- | Gets the maximum bound of the Board
 get_max :: Board -> -- ^Takes the current Board state
-			Int -- ^Returns maximum bound of Board
+           Int -- ^Returns maximum bound of Board
 get_max board = (size board) `quot` 2
 
 -- | Checks to see if the position clicked maps to a valid board space
 validMove :: Board -> -- ^Takes current Board state
-				Position -> -- ^Position current clicked
-				Bool -- ^True if valid, else False id invalid
+             Position -> -- ^Position current clicked
+             Bool -- ^True if valid, else False id invalid
 validMove board (x, y) = x >= (get_min board) && y >= (get_min board) && x <= (get_max board) && y <= (get_max board)
 
 -- | Undoes the supplied number of moves
 undo :: Int -> -- ^How many moves you want to undo
-		World -> -- ^Current World state
-		World -- ^New World state with 'x' amount of moves undone
+        World -> -- ^Current World state
+        World -- ^New World state with 'x' amount of moves undone
 undo n w = foldr (.) id (replicate n undoOne) w
 
 -- | Undoes a single move
 undoOne :: World -> -- ^Takes current World
-			World -- ^Returns new World proir to current World
+           World -- ^Returns new World proir to current World
 undoOne w = do let (newmoves, newturn) = if length currentmoves > 0
                                             then (tail currentmoves, switch currentturn)
                                             else (currentmoves, currentturn)
@@ -261,7 +261,7 @@ undoOne w = do let (newmoves, newturn) = if length currentmoves > 0
 
 -- | Checks if the settings have been initialised
 settings_complete :: Settings -> -- ^Takes current Settings applied
-						Bool -- ^Returns a bool indicating Settings addition or not
+                     Bool -- ^Returns a bool indicating Settings addition or not
 settings_complete s = if ((grid_size s) /= 0) && 
                          ((target_size s) /= 0) && 
                          ((ai_difficulty s) /= 0) && 
