ff2c05ec25a95b651efac5a64c086cc7c8bf5b17
bc37 <bc37@st-andrews.ac.uk>
2015-04-20 14:18:22 +0100

    Added documentation for 'Input.hs' and some minor code layout changes in the other committed files

--- src/AdvancedAI.hs
+++ src/AdvancedAI.hs
@@ -8,7 +8,7 @@ import Data.Ord
 -- | List comprehension to examine all positions on the board and
 -- build a list of unused positions. Availability is checked by way
 -- of the 'contains' method.
-get_possible_moves :: Board -> -- ^ Current board.
+get_possible_moves :: Board -> -- ^ Current 'Board'.
                       Colour -> -- ^ Player to check for.
                       [Position] -- ^ List of positions.
 get_possible_moves board colour = [ (x, y) 
@@ -25,8 +25,8 @@ get_possible_moves board colour = [ (x, y)
 -- that have to be evaluated. Can also be ordered such that the moves at the start 
 -- of the list are the ones most likely to be successful, as this will result in 
 -- the AI being able to prune more effectively.
-get_considered_moves :: Board -> -- ^Takes current Board state
-                        Colour -> -- ^Player Colour to check for
+get_considered_moves :: Board -> -- ^Takes current 'Board' state
+                        Colour -> -- ^Player 'Colour' to check for
                         [Position] -- ^Returns list of positions for the given colour to consider
 get_considered_moves board colour = if null (pieces board) then [(0, 0)] else nub 
                                        [doTrans pos trans | pos <- pieces board, trans <- 
@@ -37,12 +37,12 @@ get_considered_moves board colour = if null (pieces board) then [(0, 0)] else nu
           doubleTrans (t_x, t_y) = (2 * t_x, 2 * t_y)
           unoccupied pos trans = not (contains (doTrans pos trans) (pieces board))
 
--- | Gets the largest Int for alpha-beta pruning, 'infinity'.
+-- | Gets the largest Int for alpha-beta pruning, infinity.
 infty :: Int
 infty = maxBound :: Int
 
 -- | Gets the best move for the AI for a specified depth to traverse the tree.
-getbestmove :: Board -> -- ^Takes the current Board state
+getbestmove :: Board -> -- ^Takes the current 'Board' state
                Int -> -- ^The depth to traverse down the tree
                Colour -> -- ^The colour to get the best move for
                Position -- ^The final 'best' position
@@ -50,8 +50,8 @@ getbestmove board depth colour = getpos (alphabeta board colour depth (-infty) i
     where getpos (pos, _) = pos
 
 -- | Alpha-Beta Pruning algorithm that traverses the tree of 
--- possible Board states
-alphabeta :: Board -> -- ^Takes the current Board state
+-- possible 'Board' states
+alphabeta :: Board -> -- ^Takes the current 'Board' state
              Colour -> -- ^To indicate the current turn
              Int -> -- ^How far down to search the tree
              Int -> -- ^The value of alpha
@@ -71,7 +71,7 @@ alphabeta board current_turn depth alpha beta maximising_colour
 
 -- | Function that returns the best move tuple for a player based on the depth of 
 -- traversal passed in
-maximise :: Board -> -- ^Takes the current Board state
+maximise :: Board -> -- ^Takes the current 'Board' state
             [Position] -> -- ^List of all possible moves
             Colour -> -- ^The current player's colour
             Int -> -- ^Depth to traverse for maximisation
@@ -96,7 +96,7 @@ maximise board (pos:poss) current_turn depth value alpha beta maximising_colour
 
 -- | Function that returns the worst move tuple for a player based on the depth of 
 -- traversal passed in
-minimise :: Board -> -- ^Takes the current Board state
+minimise :: Board -> -- ^Takes the current 'Board' state
             [Position] -> -- ^List of all possible moves
             Colour -> -- ^The current player's colour
             Int -> -- ^Depth to traverse for minimisation
@@ -121,9 +121,9 @@ minimise board (pos:poss) current_turn depth value alpha beta maximising_colour
 
 -- | An evaluation function for a minimax search. Given a board and a colour
 -- return an integer indicating how good the board is for that colour.
-evaluate :: Board -> -- ^Takes the current Board state
-            Colour -> -- ^Player colour that you want to evaluate the Board for
-            Int -- ^Returns a value based on how good the Board is for the player
+evaluate :: Board -> -- ^Takes the current 'Board' state
+            Colour -> -- ^Player colour that you want to evaluate the 'Board' for
+            Int -- ^Returns a value based on how good the 'Board' is for the player
 evaluate b col =   (sum [(countNConnected b n col) * (weight n)
                       | n <- [(target b), (target b) - 1 .. 1]]) `quot` 2  +
                          sum [(countNConnected b n (switch col)) * (-weight n)
@@ -132,20 +132,20 @@ evaluate b col =   (sum [(countNConnected b n col) * (weight n)
 
 -- | Makes an AI move, based on the best result from tree, and returns
 -- a maybe board if successful.
-move_ai :: World -> -- ^Takes the current World state
-           Board -> --The current Board state
+move_ai :: World -> -- ^Takes the current 'World' state
+           Board -> -- ^The current 'Board' state
            Colour -> -- ^The colour of the AI
-           Maybe Board -- ^The new Board state with the AI move, or Nothing
+           Maybe Board -- ^The new 'Board' state with the AI move, or Nothing
 move_ai world board colour = makeMove board colour (getbestmove board 4 (turn world))
 
 -- | AI world, resulting from an AI move.
-get_ai_world :: World -> World -- ^ New updated world.
+get_ai_world :: World -> World -- ^ New updated 'World'.
 get_ai_world world = maybeBoardToWorld world (move_ai world (board world) (turn world))
 
 -- | Update the world state after some time has passed.
 updateWorld :: Float -- ^Time since last update.
-            -> World -- ^Current world state.
-            -> World -- ^New world state.
+            -> World -- ^Current 'World' state.
+            -> World -- ^New 'World' state.
 updateWorld time world
   | turn world == (human world) = world -- This is the users turn.
   | otherwise       = case won (board world) of 
